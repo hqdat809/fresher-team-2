a51050afb4248d93f3944e0c401e8db2b85f05ba
hqdat809 <dathq@eupgroup.net>
2023-12-20 15:10:33 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import "./index.css";
 import React from "react";
-import App from "./App.js";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import configureStore from "./stores/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
 
 const { store, persistor } = configureStore();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
