6e6aac8534347f479a19d2047e067365d3877344
hqdat809 <dathq@eupgroup.net>
2023-12-21 08:52:20 +0700

    test git merge

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -120,7 +120,7 @@ const TestPage = () => {
       <Layout className="layout-default layout-signin">
         <Header>
           <div className="header-col header-brand">
-            <h5>Dashboard</h5>
+            <h5>Dashboard 2</h5>
           </div>
         </Header>
         <Content className="signin">
